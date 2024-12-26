e90ebb49f6475dc214e88df77f1eefeda2c46d52
jezreelfasa <emmanuelfasa@gmail.com>
2024-12-26 09:41:22 +0100

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -8,12 +8,12 @@ const projectsData = [
   
   {
     id: 1,
-    title: "GPT-3 Web App",
-    description: "A React component showcasing GPT-3's capabilities and applications using UI/UX images and brief feature highlights. Built with React and JavaScript.",
-    image: "/images/ai.png",
+    title: "Job Search App",
+    description: "This is a job search app built with react native, with login functionality using firebase as backend. API was also integrated in this app to fetch job search results real-time.",
+    image: "/images/num4.png",
     tag: ["All", "Web"],
-    gitUrl: "https://github.com/jezreelfasa/gpt-3_app.git",
-    previewUrl: "https://gpt-3-app-eight.vercel.app",
+    gitUrl: "https://github.com/jezreelfasa/reacty-native_job_search_app.git",
+    previewUrl: "https://expo.dev/preview/update?message=Job%20Search%20App%20built%20with%20react%20native&updateRuntimeVersion=1.0.0&createdAt=2024-12-25T15%3A16%3A59.705Z&slug=exp&projectId=2bb2ab32-d60c-403f-9d3f-0ec55642884a&group=60b4bd90-4f3a-48b8-b35c-ec10adea3a7f",
 
 
   },
@@ -21,89 +21,15 @@ const projectsData = [
   
   {
     id: 2,
-    title: "Hoobank Web App",
-    description: "The Money App, a single page React.js-based platform, offers streamlined sections like Billing, Business, Card Deals etc. Users can effortlessly manage billing, handle financial transactions, and find optimal card deals. Each section boasts intuitive interfaces for a seamless user experience.",
-    image: "/images/money.svg",
+    title: "Todo App",
+    description: " Todo app built with react native, firebase as backend, login and signup functionality. User has options from the home screen to either go to the todo app or visit my github repo or just search for jobs with the job app I uilt.",
+    image: "/images/native.png",
     tag: ["All", "Web"],
-    gitUrl: "https://github.com/jezreelfasa/money_pay_app.git",
-    previewUrl: "https://money-pay-app.vercel.app",
-  },
-  
-  {
-    id: 3,
-    title: "Restaurant Web",
-    description: "A React.js restaurant app which offers a visually appealing interface with key sections. It effectively communicates the restaurant's essence, location, and menu highlights, enticing visitors to explore further and potentially visit the establishment.",
-    image: "/images/newreest.png",
-    tag: ["All", "Web"],
-    gitUrl: "https://github.com/jezreelfasa/my_restaurant_web.git",
-    previewUrl: "https://my-restaurant-web.vercel.app",
+    gitUrl: "https://github.com/jezreelfasa/native_Todo_App.git",
+    previewUrl: "https://expo.dev/preview/update?message=Todo%20app%20(changes%20made%20to%20the%20_layout.js%20to%20make%20login%20initialRouteName&updateRuntimeVersion=1.0.0&createdAt=2024-12-24T21%3A15%3A10.431Z&slug=exp&projectId=b3dafa3b-0293-4a9b-ad58-98c761f7a771&group=bc7a5a6b-339f-4347-97b1-7641becc9c22",
   },
   
   
-  
-  {
-    id: 4,
-    title: "To-do App",
-    description: "Developed a to-do app using React.js, utilizing local storage with JSON.parse for data storage. This app enables end users to manage tasks with features like add, edit, and delete tasks.",
-    image: "/images/todo.png",
-    tag: ["All","Task & Knowledge"],
-    gitUrl: "https://github.com/jezreelfasa/to-do.git",
-    previewUrl: "https://to-do-ten-beta.vercel.app",
-  },
-  
-  {
-    id: 5,
-    title: "e-Commerce Shoe App:",
-    description: "Built a functional mini e-Commerce app for shoe shopping using React.js. Implemented search functionality, allowing users to navigate through products and view prices. Enhanced user experience with responsive design and intuitive navigation.",
-    image: "/images/shoe.png",
-    tag: ["All", "Web"],
-    gitUrl: "https://github.com/jezreelfasa/shoes_web.git",
-    previewUrl: "https://shoes-web-vert.vercel.app",
-  },
-  
-  {
-    id: 6,
-    title: "Quiz App",
-    description: "Created a functional quiz app with a custom question bank, Implemented user-friendly features for quiz navigation and scoring Utilizing React.js  for the front-end logic.",
-    image: "/images/billionaire.png",
-    tag: ["All", "Task & Knowledge"],
-    gitUrl: "https://github.com/jezreelfasa/millionaire-quiz-by-emmanuel.git",
-    previewUrl: "https://millionaire-quiz-by-emmanuel.vercel.app",
-  },
-
-  {
-    id: 7,
-    title: "Drive Web",
-    description: "Developed a web app displaying various cars with login and sign-up functionality. Designed an aesthetically pleasing slide feature for an engaging user experience.",
-    image: "/images/driver.png",
-    tag: ["All", "Web"],
-    gitUrl: "https://github.com/jezreelfasa/cars.webs.git",
-    previewUrl: "https://cars-webs.vercel.app",
-  },
-
-
-  {
-    id: 8,
-    title: "E-commercer",
-    description: "A test E-commerce app showcasing various brands and designs of clothings with functional navigations from page to page, build with HTML, CSS and JavaScript.",
-    image: "/images/ecommer.svg",
-    tag: ["All", "Web"],
-    gitUrl: "https://github.com/jezreelfasa/shop.web.git",
-    previewUrl: "https://jezreelfasa.github.io/shop.web/",
-  },
-
-
-  {
-    id: 8,
-    title: "AWS DevOps Projects",
-    description: "Diverse AWS DevOps projects, leveraging ECS, RDS, IAM, EC2, S3, Route 53, Active Directory, SNS, Kubernetes, MySQL, Docker, Jenkins, Python, Git, and GitHub. From orchestrating containerized applications on ECS to automating CI/CD pipelines with Jenkins, efficient resource management, secure access controls, and seamless integration of AWS services. Utilizing Git and GitHub to ensure version control, and streamlined collaboration, facilitating code sharing across distributed teams, enhancing overall development lifecycle.",
-    image: "/images/awsd.png",
-    tag: ["All", "AWS DevOps"],
-    gitUrl: "https://github.com/jezreelfasa?tab=repositories",
-    previewUrl: "https://odubuemmanuel.wordpress.com",
-  },
-
-
   
   
 ];
